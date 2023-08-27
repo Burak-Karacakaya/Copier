04490b804ccea9fa4a4571337ba8260a93545f48
Burak-Karacakaya <burakkaracakaya@yahoo.com>
2023-08-27 23:03:18 +0300

    added commandoptions, added FileSystemWatcher

---
+++ Client/CommandOptions.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Client
+{
+    public class CommandOptions
+    {
+        [Option('f', "fileGlobPattern", Required = true, HelpText = "Files to be searched.")]
+        public string FileGlobalPattern { get; set; }
+
+        [Option('d', "destionationDirectoryPath", Required = true, HelpText = "Destination directory path")]
+        public string DestinationDirectoryPath { get; set; }
+
+        [Option('s', "sourceDirectoryPath", HelpText = "Parent directory where the files will be..")]
+        public string SourceDirectoryPath { get; set; }
+
+        [Usage]
+        public static IEnumerable<Example> Examples => new List<Example>()
+        {
+            new Example("Start the copier", new UnParserSettings(){PreferShortName = true},
+                new CommandOptions
+                {
+                    SourceDirectoryPath = "C://Users/MyDocuments/Images",
+                    FileGlobalPattern = "*.jpg",
+                    DestinationDirectoryPath = "C:/Users/MyDocuments/NewImages"
+                })
+        };
+
+
+    }
+}
+
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Client;
+using CommandLine;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+
+//Copier "specify location", copy to "target folder"
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<CommandOptions>(args)
+            .WithParsed(StartWatching)
+            .WithNotParsed(a =>
+            {
+                Environment.Exit(1);
+            });
+    }
+
+    private static void StartWatching(CommandOptions options)
+    {
+        var files = GetMatchingFiles(options);
+        var file = "";
+        var watcher = new FileSystemWatcher
+        {
+            Path = options.SourceDirectoryPath,
+            NotifyFilter = NotifyFilters.Size | NotifyFilters.LastWrite | NotifyFilters.FileName,
+            Filter = file
+        };
+
+        watcher.Changed += (sender, args) => Console.WriteLine("File has changed");
+        watcher.Renamed += (sender, args) => Console.WriteLine("File has been renamed");
+
+        //Start watching the file.
+        watcher.EnableRaisingEvents = true;
+
+
+        Console.WriteLine(files.Select(a => a.Path).Aggregate((a, b) => a + ", " + b));
+    }
+
+
+
+    private static IEnumerable<FilePatternMatch> GetMatchingFiles(CommandOptions options)
+    {
+        var m = new Matcher();
+        m.AddInclude(options.FileGlobalPattern);
+
+        var directoryInfo = new DirectoryInfo(string.IsNullOrWhiteSpace(options.SourceDirectoryPath)
+            ? Directory.GetCurrentDirectory() : options.SourceDirectoryPath);
+        var dirInfo = new DirectoryInfoWrapper(directoryInfo);
+
+        var files = m.Execute(dirInfo).Files;
+        return files;
+    }
+}
 
